6b59bb8dc08383c70aa1435516ad8122b7218029
Felipe Martins <mfelipeluiz99@gmail.com>
2022-07-24 14:31:42 -0300

    feat: adding mocks for testing porpouses, adding global variables themes and small fixes at app/index

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Layout } from './components/layout';
+
+import {
+  SearchPage,
+  RepositoriesPage,
+  BranchesPage,
+  CommitsPage,
+} from './components/page';
+
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<SearchPage />} />
+      <Route path='' element={<Layout />}>
+        <Route path='/repositories/:name' element={<RepositoriesPage />} />
+        <Route path='/branches/:name/:repo' element={<BranchesPage />} />
+        <Route path='/branches/commits/:name/:repo' element={<CommitsPage />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/mocks/mocks.ts
@@ -0,0 +1,315 @@
+// https://api.github.com/users/Felpsmars/repos listagem de repos
+// https://api.github.com/repos/Felpsmars/Cupcaker-Challenge/branches listagem de branchs
+// https://api.github.com/repos/Felpsmars/Cupcaker-Challenge/commits listagem de commits referente a uma branch
+
+const sidebarMock = {
+  name: 'Felipe Martins',
+  login: 'Felpsmars',
+  avatar_url: 'https://avatars.githubusercontent.com/u/85464159?v=4',
+  bio: 'Do Direito, para cozinha e agora na programação aonde finalmente me encontrei e quero seguir minha vida.',
+};
+
+const branchesMock = [
+  {
+    name: 'fake Repo Name',
+  },
+  {
+    name: 'fake Repo Name',
+  },
+  {
+    name: 'fake Repo Name',
+  },
+  {
+    name: 'fake Repo Name',
+  },
+  {
+    name: 'fake Repo Name',
+  },
+  {
+    name: 'fake Repo Name',
+  },
+  {
+    name: 'fake Repo Name',
+  },
+  {
+    name: 'fake Repo Name',
+  },
+  {
+    name: 'fake Repo Name',
+  },
+  {
+    name: 'fake Repo Name',
+  },
+  {
+    name: 'fake Repo Name',
+  },
+  {
+    name: 'fake Repo Name',
+  },
+];
+
+const commitsMock = [
+  {
+    name: 'felipe',
+    author: {
+      login: 'felpsmars',
+    },
+    commit: {
+      message: 'fake message',
+    },
+  },
+  {
+    name: 'felipe',
+    author: {
+      login: 'felpsmars',
+    },
+    commit: {
+      message: 'fake message',
+    },
+  },
+  {
+    name: 'felipe',
+    author: {
+      login: 'felpsmars',
+    },
+    commit: {
+      message: 'fake message',
+    },
+  },
+  {
+    name: 'felipe',
+    author: {
+      login: 'felpsmars',
+    },
+    commit: {
+      message: 'fake message',
+    },
+  },
+  {
+    name: 'felipe',
+    author: {
+      login: 'felpsmars',
+    },
+    commit: {
+      message: 'fake message',
+    },
+  },
+  {
+    name: 'felipe',
+    author: {
+      login: 'felpsmars',
+    },
+    commit: {
+      message: 'fake message',
+    },
+  },
+  {
+    name: 'felipe',
+    author: {
+      login: 'felpsmars',
+    },
+    commit: {
+      message: 'fake message',
+    },
+  },
+  {
+    name: 'felipe',
+    author: {
+      login: 'felpsmars',
+    },
+    commit: {
+      message: 'fake message',
+    },
+  },
+  {
+    name: 'felipe',
+    author: {
+      login: 'felpsmars',
+    },
+    commit: {
+      message: 'fake message',
+    },
+  },
+  {
+    name: 'felipe',
+    author: {
+      login: 'felpsmars',
+    },
+    commit: {
+      message: 'fake message',
+    },
+  },
+];
+
+const repositoriesMock = [
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+  {
+    id: 1111,
+    name: 'repo 1',
+  },
+];
+
+export { sidebarMock, branchesMock, commitsMock, repositoriesMock };
